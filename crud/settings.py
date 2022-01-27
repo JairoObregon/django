024ae5565e7d2e7ca504f7ec43cf03f861e0d161
jairo <jairo.obregon@unmsm.edu.pe>
2022-01-26 23:23:24 -0500

    asd

--- crud/settings.py
+++ crud/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(mxyvk)(eyy4&34qtojno!y6oem8ki0&wchze(*oy5+rx*-(5e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,5 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # Configure Django App for Heroku.
-import django_heroku
+
 django_heroku.settings(locals())
