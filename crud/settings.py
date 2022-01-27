f16e3a99469c1e95cc5e48b34c922533c1f45bd5
jairo <jairo.obregon@unmsm.edu.pe>
2022-01-26 22:59:07 -0500

    first commit

--- crud/settings.py
+++ crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(mxyvk)(eyy4&34qtojno!y6oem8ki0&wchze(*oy5+rx*-(5e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
